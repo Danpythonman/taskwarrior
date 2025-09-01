81906254094b032cdb68800311c447c2c2c9dc08
Daniel Di Giovanni <dannyjdigio@gmail.com>
2025-09-01 01:02:25 -0400

    Added CORS to backend

--- backend/server.py
+++ backend/server.py
@@ -3,13 +3,21 @@ import json
 import signal
 
 from fastapi import FastAPI, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import JSONResponse
 
 
 CMD = ['task', 'status:pending', 'export']
 
-
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get('/tasks')
